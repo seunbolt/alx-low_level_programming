99c7d18fd056ef2b71bfce78657cf8f2ced30308
Adebayo Oluwaseun <seunbolt@gmail.com>
2023-07-18 15:56:52 -0700

    print sign and return

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign of a number
+ * @n: integer parameter can be - or +
+ * Description: print +, -, 0 depending on number
+ * Return: 1, 0, -
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
