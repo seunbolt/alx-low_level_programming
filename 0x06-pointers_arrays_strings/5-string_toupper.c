57196fae15a9c6f4fc681ba04f398fb849e792ae
Adebayo Oluwaseun <seunbolt@gmail.com>
2023-07-26 15:42:17 -0700

    change lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,10 @@ char *string_toupper(char *s)
 {
 	int i = 0;
 
-	while (  s[i] != '\0')
+	while (s[i] != '\0')
 	{
-		if ( (s[i] >= 'a') && (s[i] <= 'z'))
-		      s[i] = s[i] - 32;
+		if ((s[i] >= 'a') && (s[i] <= 'z'))
+			s[i] = s[i] - 32;
 		i++;
 	}
 
