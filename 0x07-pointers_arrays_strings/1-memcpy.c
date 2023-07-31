c92562c497f09dd0e72945f682c4e6829564bbd3
Adebayo Oluwaseun <seunbolt@gmail.com>
2023-07-31 16:40:20 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy -  copies n bytes from memory area src,
+ * to memory area dest
+ * @dest: char destination
+ * @src: char source
+ * @n: integer number
+ * Return: return pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+
+}
