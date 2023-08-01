73ebca0fbfa18e702ebb07f423460ccd4c5653de
Adebayo Oluwaseun <seunbolt@gmail.com>
2023-07-31 18:50:22 -0700

    _strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -18,9 +18,10 @@ char *_strpbrk(char *s, char *accept)
 		for (j = 0 ; *(accept + j) != 0 ; j++)
 		{
 			if (*(s + i) == *(accept + j))
-					return (s + i);
+				return (s + i);
 		}
 	i++;
 	}
+
 	return ('\0');
 }
