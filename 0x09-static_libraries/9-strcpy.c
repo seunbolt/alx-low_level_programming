3018664a4a1cbaad0973425a83ad855fb0d7b390
Adebayo Oluwaseun <seunbolt@gmail.com>
2023-08-04 17:18:41 -0700

    my static library

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,20 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * _strncpy - copies a string
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte, to the
+ * buffer pointed to by dest.
  * @dest: destination.
  * @src: source.
- * @n: amount of bytes from src.
  * Return: the pointer to dest.
  */
-
-char *_strncpy(char *dest, char *src, int n)
+char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int count = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
-
+	while (count >= 0)
+	{
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
+	}
 	return (dest);
 }
