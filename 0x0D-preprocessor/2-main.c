89372d0ff80e9e5d94c008169d5ccd6b489a11ba
Adebayo Oluwaseun <seunbolt@gmail.com>
2023-08-14 14:52:15 -0700

    print file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+/**
+ * main - entry point
+ * progrm that prints its own name
+ *
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
