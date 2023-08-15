ffd598d760f6a5c85521367e030f62534174caaf
Adebayo Oluwaseun <seunbolt@gmail.com>
2023-08-15 11:36:06 -0700

    print structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,13 +16,13 @@ void print_dog(struct dog *d)
 	{
 
 	if ((*d).name == NULL)
-		printf("Name: nil\n");
+		printf("Name: (nil)\n");
 	printf("Name: %s\n", (*d).name);
 
 	printf("Age: %f\n", (*d).age);
 
 	if ((*d).owner == NULL)
-		printf("Owner: nil\n");
+		printf("Owner: (nil)\n");
 	printf("Owner: %s\n", (*d).owner);
 	}
 
