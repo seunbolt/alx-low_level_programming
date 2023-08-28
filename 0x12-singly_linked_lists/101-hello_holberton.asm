b3ff60d2268c9d5901a0a4a709d29fb0e9f53f00
Adebayo Oluwaseun <seunbolt@gmail.com>
2023-08-28 15:08:43 -0700

    singly linked list

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,22 @@
-section .data
-hello db "Hello, Holberton", 10 ; The string to print, null-terminated
-format db "%s", 0 		; The format string for printf, null-terminated
+
+n .data
+hello db "Hello, Holberton", 0
+format db "%s", 10, 0
 
 section .text
 global main
 extern printf
 
 main:
-push rbp			; Save the base pointer
-mov rdi, format			; Load the address of the format string into rdi (1st argument)
-mov rsi, hello			; Load the address of the hello string into rsi (2nd argument)
-call printf			; Call the printf function
-pop rbp				; restore the base pointer
-ret				; return from the main function
+sub rsp, 8     ; Align stack
+push rdi       ; Preserve rdi register
+
+mov rdi, format
+mov rsi, hello
+call printf
+
+pop rdi        ; Restore rdi register
+add rsp, 8     ; Restore stack alignment
+
+ret
+
