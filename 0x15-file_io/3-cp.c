02e5a71fc86dd7d0070dfe7793381a5b9173ba2d
Adebayo Oluwaseun <seunbolt@gmail.com>
2023-09-06 09:43:20 -0700

    copy text to a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,67 +1,54 @@
 #include "main.h"
 
-int copy_text_file(const char *file_from, const char *file_to);
 /**
  * main - entry point of the program
- * @ac: argument counter
- * @av: argument vector
+ * @argc: argument counter
+ * @argv: argument vector
  *
  * Return: always 0 success
  */
 int main(int argc, char *argv[])
 {
+	int source, dest;
+	ssize_t bytes_read, write_bytes;
+	char buffer[1024];
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	copy_text_file(argv[1], argv[2]);
-
-	return (0);
-}
-
-/**
- * copy_text_file - copy content of file to another file
- * @file_from: source file
- * @file_to: destination file
- *
- * Return: 1 if successful
- */
-int copy_text_file(const char *file_from, const char *file_to)
-{
-	int source, dest;
-	ssize_t bytes_read, write_bytes;
-	char buffer[1024];
-
-	source = open(file_from, O_RDONLY);
+	source = open(argv[1], O_RDONLY);
 	if (source == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
 		exit(98);
 	}
-	dest = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (dest == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Cant't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Cant't write to %s\n", argv[2]);
 		exit(99);
 	}
 	/*read from file_from and write to file_to*/
-	while ((bytes_read = read(source, buffer, 1024)) > 0)
+	bytes_read = read(source, buffer, sizeof(buffer));
+	while (bytes_read > 0)
 	{
 		if (bytes_read == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from %s\n",
-					file_from);
+			dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
 			exit(98);
 		}
+		if (bytes_read == 0) /*end of file reached*/
+			break;
+
 		write_bytes = write(dest, buffer, bytes_read);
 		if (write_bytes == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
-					file_to);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-		bytes_read = read(source, buffer, 1024);
+		bytes_read = read(source, buffer, sizeof(buffer));
 	}
 	close(source);
 	close(dest);
@@ -71,6 +58,5 @@ int copy_text_file(const char *file_from, const char *file_to)
 		exit(100);
 	}
 	return (0);
-
 }
 
